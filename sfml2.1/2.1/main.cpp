25b1769d71e61cc1b3ed855ebb33e140787913a2
AlexDor <32705886+AlexDorohoff@users.noreply.github.com>
2018-07-04 23:19:12 +0300

    небольшой глюк остался

--- sfml2.1/2.1/main.cpp
+++ sfml2.1/2.1/main.cpp
@@ -33,17 +33,37 @@ struct PRNG
 
 void initGenerator(PRNG &generator)
 {
+    // Создаём псевдо-устройство для получения случайного зерна.
     std::random_device device;
+    // Получаем случайное зерно последовательности
     generator.engine.seed(device());
+}
+
+// Генерирует целое число в диапазоне [minValue, maxValue)
+unsigned random(PRNG &generator, unsigned minValue, unsigned maxValue)
+{
+    // Проверяем корректность аргументов
+    assert(minValue < maxValue);
+
+    // Создаём распределение
+    std::uniform_int_distribution<unsigned> distribution(minValue, maxValue);
+
+    // Вычисляем псевдослучайное число: вызовем распределение как функцию,
+    //  передав генератор произвольных целых чисел как аргумент.
+    return distribution(generator.engine);
 }
 
 // Генерирует число с плавающей точкой в диапазоне [minValue, maxValue)
 float getRandomFloat(PRNG &generator, float minValue, float maxValue)
 {
+    // Проверяем корректность аргументов
     assert(minValue < maxValue);
 
+    // Создаём распределение
     std::uniform_real_distribution<float> distribution(minValue, maxValue);
 
+    // Вычисляем псевдослучайное число: вызовем распределение как функцию,
+    //  передав генератор произвольных целых чисел как аргумент.
     return distribution(generator.engine);
 }
 
@@ -60,7 +80,6 @@ void initBall(Ball (&ball)[maxNumberOfBalls], BallParameters (&parametrs)[maxNum
     for (int i = 0; i < maxNumberOfBalls; i++)
     {
         ball[i].shape.setRadius(parametrs[i].radius);
-        ball[i].shape.setOrigin(parametrs[i].radius, parametrs[i].radius);
         ball[i].shape.setFillColor(parametrs[i].color);
         ball[i].shape.setPosition(parametrs[i].startPosition);
         ball[i].speed = ballSpeed(generator);
@@ -69,14 +88,14 @@ void initBall(Ball (&ball)[maxNumberOfBalls], BallParameters (&parametrs)[maxNum
 
 void getBallParametrs(BallParameters (&array)[maxNumberOfBalls])
 {
-    array[0] = {10.f, sf::Color(0x99, 0x33, 0x66), sf::Vector2f{280, 320}};
-    array[1] = {70.f, sf::Color(0xFF, 0x99, 0xCC), sf::Vector2f{20, 100}};
-    array[2] = {34.f, sf::Color(0xFF, 0x00, 0x00), sf::Vector2f{250, 350}};
+    array[0] = {10.f, sf::Color(0x99, 0x33, 0x66), sf::Vector2f{320, 320}};
+    array[1] = {20.f, sf::Color(0xFF, 0x99, 0xCC), sf::Vector2f{460, 100}};
+    array[2] = {25.f, sf::Color(0x00, 0x00, 0xFF), sf::Vector2f{200, 100}};
     array[3] = {30.f, sf::Color(0x33, 0x33, 0x99), sf::Vector2f{400, 300}};
     array[4] = {40.f, sf::Color(0x00, 0xFF, 0xFF), sf::Vector2f{500, 500}};
-    array[5] = {35.f, sf::Color(0xFF, 0xFF, 0x00), sf::Vector2f{600, 400}};
+    array[5] = {35.f, sf::Color(0xFF, 0xFF, 0x00), sf::Vector2f{600, 500}};
     array[6] = {25.f, sf::Color(0xFF, 0x99, 0x00), sf::Vector2f{100, 400}};
-    array[7] = {15.f, sf::Color(0x00, 0x00, 0xFF), sf::Vector2f{50, 150}};
+    array[7] = {15.f, sf::Color(0xFF, 0x00, 0x00), sf::Vector2f{250, 400}};
     array[8] = {20.f, sf::Color(0xFF, 0xFF, 0xFF), sf::Vector2f{250, 380}};
     array[9] = {10.f, sf::Color(0x96, 0x96, 0x96), sf::Vector2f{100, 200}};
 }
@@ -93,51 +112,25 @@ void setBallNewPosition(Ball (&arrayOfBalls)[maxNumberOfBalls], sf::Clock &clock
         if ((position.x + 2 * arrayOfBalls[i].shape.getRadius() >= WINDOW_WIDTH) && (arrayOfBalls[i].speed.x > 0))
         {
             arrayOfBalls[i].speed.x = -arrayOfBalls[i].speed.x;
+            std::cout << "blonk!/n";
         }
         if ((position.x < 0) && (arrayOfBalls[i].speed.x < 0))
         {
             arrayOfBalls[i].speed.x = -arrayOfBalls[i].speed.x;
+            std::cout << "blonk!/n";
         }
         if ((position.y + 2 * arrayOfBalls[i].shape.getRadius() >= WINDOW_HEIGHT) && (arrayOfBalls[i].speed.y > 0))
         {
             arrayOfBalls[i].speed.y = -arrayOfBalls[i].speed.y;
+            std::cout << "blonk!/n";
         }
         if ((position.y < 0) && (arrayOfBalls[i].speed.y < 0))
         {
             arrayOfBalls[i].speed.y = -arrayOfBalls[i].speed.y;
+            std::cout << "blonk!/n";
         }
 
         arrayOfBalls[i].shape.setPosition(position);
-    }
-}
-
-float length(const sf::Vector2f &vector)
-{
-    return std::hypot(vector.x, vector.y);
-}
-
-void checkBallsClash(Ball (&arrayOfBalls)[maxNumberOfBalls], sf::Clock &clock)
-{
-    const float deltaTime = clock.restart().asSeconds();
-
-    for (int i = 0; i < maxNumberOfBalls; ++i)
-    {
-        for (int j = i + 1; j < maxNumberOfBalls; ++j)
-        {
-            if (length(arrayOfBalls[i].shape.getPosition() - arrayOfBalls[j].shape.getPosition()) <= (arrayOfBalls[i].shape.getRadius() + arrayOfBalls[j].shape.getRadius()))
-            {
-                sf::Vector2f deltaI = arrayOfBalls[i].shape.getPosition() - arrayOfBalls[j].shape.getPosition();
-                sf::Vector2f speedI = arrayOfBalls[i].speed - arrayOfBalls[j].speed;
-
-                sf::Vector2f deltaJ = arrayOfBalls[j].shape.getPosition() - arrayOfBalls[i].shape.getPosition();
-                sf::Vector2f speedJ = arrayOfBalls[j].speed - arrayOfBalls[i].speed;
-
-                arrayOfBalls[i].speed = arrayOfBalls[i].speed -
-                                        ((speedI.x * deltaI.x + speedI.y * deltaI.y) / (deltaI.x * deltaI.x + deltaI.y * deltaI.y)) * deltaI;
-                arrayOfBalls[j].speed = arrayOfBalls[j].speed -
-                                        ((speedJ.x * deltaJ.x + speedJ.y * deltaJ.y) / (deltaJ.x * deltaJ.x + deltaJ.y * deltaJ.y)) * deltaJ;
-            }
-        }
     }
 }
 
@@ -184,7 +177,6 @@ int main()
         sf::Event event;
 
         setBallNewPosition(arrayOfBalls, clock);
-        checkBallsClash(arrayOfBalls, clock);
         window.clear();
         drawBalls(arrayOfBalls, window);
         window.display();
